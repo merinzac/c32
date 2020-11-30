090d6df6e8778c64a07b6b5ef0dfaf31ff14551c
merinzac <70364261+merinzac@users.noreply.github.com>
2020-11-30 20:03:48 +0530

    Add files via upload

--- Slingshot.js
+++ Slingshot.js
@@ -13,10 +13,7 @@ class SlingShot{
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
     }
-    attach(body){
-        this.sling.bodyA = body;
-    }
-    
+
     fly(){
         this.sling.bodyA = null;
     }
